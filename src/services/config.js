5cba59abb0616723812eea6a6b2322e057f15291
Deokhaeng Lee <jefflee.ldh@gmail.com>
2025-04-27 19:18:34 -0400

    ready for vercel

---
+++ src/services/config.js
@@ -0,0 +1,6 @@
+console.log('Environment variables:', {
+  REACT_APP_API_URL: process.env.REACT_APP_API_URL,
+  NODE_ENV: process.env.NODE_ENV
+});
+
+export const API_URL = process.env.REACT_APP_API_URL;
